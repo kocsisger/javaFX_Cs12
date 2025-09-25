fed2837971c2f040db4f55c92db118a5ffd5b10d
Kocsis Gergely <kocsisger@example.com>
2025-09-25 13:18:32 +0200

    Final

--- src/main/java/org/example/MainController.java
+++ src/main/java/org/example/MainController.java
@@ -11,7 +11,10 @@ public class MainController {
     @FXML
     void handleClick() {
         System.out.println("Hello world!!!");
-
+        if (seasonsLabel.getText().equals("Summer"))
+            seasonsLabel.setText("Autumn");
+        else
+            seasonsLabel.setText("Summer");
     }
 
     @FXML
